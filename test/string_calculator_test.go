9a5fc0fed0b543f0b3fdf8900b3f84b2a1b456ec
Zeynep Erden <zzeyneperdenn@gmail.com>
2022-06-24 14:49:50 +0300

    test add func with some numbers

--- test/string_calculator_test.go
+++ test/string_calculator_test.go
@@ -14,3 +14,12 @@ func Test_Add_With_Empty_String(t *testing.T) {
 
 	assert.Equal(t, 0, result)
 }
+
+func Test_Add_With_Some_Numbers(t *testing.T) {
+	result, err := internal.Add("1,2,3")
+	if err != nil {
+		t.Fail()
+	}
+
+	assert.Equal(t, 6, result)
+}
