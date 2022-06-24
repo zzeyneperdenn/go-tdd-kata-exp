7a24139bb405e45c36f4a79fd64fed995f60711a
Zeynep Erden <zzeyneperdenn@gmail.com>
2022-06-24 16:27:28 +0300

    added a func for finding delimiter

--- internal/string_calculator.go
+++ internal/string_calculator.go
@@ -6,12 +6,21 @@ import (
 )
 
 func Add(numbers string) (result int, err error) {
-	numbers = strings.Replace(numbers, "\n", ",", -1)
-	splitNumbers := strings.Split(numbers, ",")
+	delimiter := findDelimiter(numbers)
+	numbers = strings.Replace(numbers, "\n", delimiter, -1)
+	splitNumbers := strings.Split(numbers, delimiter)
 	sum := 0
 	for _, i := range splitNumbers {
 		number, _ := strconv.Atoi(i)
 		sum += number
 	}
 	return sum, nil
+}
+
+func findDelimiter(text string) (delimiter string) {
+	if strings.HasPrefix(text, "//") && len(text) >= 3 {
+		return string(text[2])
+	}
+
+	return ","
 }
